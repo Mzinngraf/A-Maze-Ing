f6ce787a5f88027e92ff26ce73ed4eb2dbc34d5d
hungryliza <53476586+hungryliza@users.noreply.github.com>
2026-04-24 08:58:25 +0100

    Update visual_display.py
    simpler display. To do : colors and path

--- A-Maze-Ing/visual_display.py
+++ A-Maze-Ing/visual_display.py
@@ -12,49 +12,52 @@ class Compass:
 
 
 class Display:
-    def __init__(self, maze, show=False, color="white", path=None):
+    def __init__(self, maze, show=False, path=None):
         self.maze = maze
         self.path = path
         self.show = show
-        self.color = color
+        self.color = {"white" : "47",
+                      "blue" : "44"}
 
 
     def render(self):
-        gridwidth = 2 * self.maze.width + 1
-        gridheight = 2 * self.maze.height + 1
-        grid = [[" " for col in range(gridwidth)] for row in range(gridheight)]
         for maze_y in range(self.maze.height):
             for maze_x in range(self.maze.width):
                 cellval = self.maze.basegrid.cells[maze_y][maze_x]
                 direction = Compass(cellval)
-                if direction.north:
-                    grid[2*maze_y][2*maze_x+1] = "─"
-                if direction.west:
-                    grid[2*maze_y+1][2*maze_x] = "|"
-                if direction.south:
-                    grid[2*maze_y+2][2*maze_x+1] = "─"
-                if direction.east:
-                    grid[2*maze_y+1][2*maze_x+2] = "|"
-                grid[2*maze_y][2*maze_x] = "+"
-                grid[2*maze_y][2*self.maze.width] = "+"
-                if (maze_x, maze_y) == self.maze.entry:
-                    grid[2*maze_y+1][2*maze_x+1] = "▶"
+                print("+", end="")
+                if maze_y == 0 or direction.north \
+                or (maze_x, maze_y) in self.maze.fortytwo \
+                or (maze_x, maze_y - 1) in self.maze.fortytwo:
+                    print("───", end="")
+                else:
+                    print("   ", end="")
+            print("+")
+
+            for maze_x in range(self.maze.width):
+                cellval = self.maze.basegrid.cells[maze_y][maze_x]
+                direction = Compass(cellval)
+                if maze_x == 0 or direction.west \
+                or (maze_x, maze_y) in self.maze.fortytwo \
+                or (maze_x - 1, maze_y) in self.maze.fortytwo:
+                    print("|", end="")
+                else:
+                    print(" ", end="")
+                if (maze_x, maze_y) in self.maze.fortytwo:
+                    print(" ✹ ", end="")
+                elif (maze_x, maze_y) == self.maze.entry:
+                    print(" ▶ ", end="")
                 elif (maze_x, maze_y) == self.maze.exit:
-                    grid[2*maze_y+1][2*maze_x+1] = "⚑"
-                elif self.show and self.path is not None and (maze_x, maze_y) in self.path:
-                    grid[2*maze_y+1][2*maze_x+1] = "▽"
-                elif (maze_x, maze_y) in self.maze.fortytwo:
-                    grid[2*maze_y+1][2*maze_x+1] = "✹" 
-                else:   
-                    grid[2*maze_y+1][2*maze_x+1] = " "
-                grid[2*maze_y+1][2*self.maze.width] = "|"
-                grid[2*self.maze.height][2*maze_x] = "+"
-                grid[2*self.maze.height][2*maze_x+1] = "─"
-                grid[2*self.maze.height][2*self.maze.width] = "+"
-        for maze_y in range(0, self.maze.height):
-            grid[2*maze_y+1][0] = "|"
-        for row in grid:
-            print(" ".join(row))
+                    print(" ⚑ ", end="")
+                else:
+                    print("   ", end="")
+            print("|")
+        for maze_x in range(self.maze.width):
+            cellval = self.maze.basegrid.cells[maze_y][maze_x]
+            direction = Compass(cellval) 
+            print("+", end="")
+            print("───", end="")
+        print("+")
 
 
 if __name__ == "__main__":
