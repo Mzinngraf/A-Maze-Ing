749875821b7c656e17de5926429b538b65ce6f0d
Marco Zinngraf <mzinngraf@gmail.com>
2026-04-24 13:41:39 +0100

    Add files via upload
    teste

---
+++ teste/A-Maze-Ing/a_maze_ing.py
@@ -0,0 +1,36 @@
+import random
+import sys
+from parser import parser
+from mazegen import MazeGenerator
+from visual_display import Display
+from algorithm import bfs
+if __name__ == "__main__":
+    maze_dict = parser()
+    gen = MazeGenerator(maze_dict["width"], maze_dict["height"], maze_dict["entry"], maze_dict["exit"])
+    maze = gen.generate()
+    path = bfs.solve_maze(gen, maze)
+    display = Display(gen, path)
+    seed = 1
+    while True:
+        display.render()
+        print("=== A-Maze-ing ===")
+        print("1. Re-generate a new maze")
+        print("2. Show/Hide path from entry to exit")
+        print("3. Rotate maze colors")
+        print("4. Quit")
+        print(path)
+        choice = input("Choice? (1-4): ")
+        if choice == "1":
+            seed += 1
+            gen.seed = seed
+            random.seed(seed)
+            gen.generate()
+        elif choice == "2":
+            if display.show is False:
+                display.show = True
+            else:
+                display.show = False
+        elif choice == "3":
+            display.color = "blue"
+        elif choice == "4":
+            sys.exit(0)
---
+++ teste/A-Maze-Ing/algorithm/dfs.py
@@ -0,0 +1,43 @@
+import random
+
+
+def gen_maze(self, grid):
+    start_y, start_x = self.entry
+    height = self.height
+    width = self.width
+    rando = random.Random(self.seed)
+    exit_x, exit_y = self.exit
+
+    stack = [(start_x, start_y)]
+    visited = {(start_x, start_y)}
+    grid = [height, width]
+
+    directions = [
+        (0, -1, 1, 4),#N
+        (1, 0, 2, 8),#L
+        (0, 1, 4, 1),#S
+        (-1, 0, 8, 2)#O
+    ]
+
+    while stack:
+        curr_x, curr_y = stack[-1]
+        unvisited_walls = []
+        for dx, dy, wall_curr, wall_next in directions:
+            nx, ny = curr_x + dx, curr_y + dy
+            if 0 <= nx < width and 0 <= ny < height and (nx, ny) not in visited:
+                unvisited_walls.append((nx, ny, wall_curr, wall_next))
+        if unvisited_walls:
+            nx, ny, wall_curr, wall_next = rando.choice(unvisited_walls)
+            grid[curr_y][curr_x] -= wall_curr
+            grid[ny][nx] -= wall_next
+            visited.add((nx, ny))
+            stack.append((nx, ny))
+        else:
+            stack.pop()
+    if self.perfect is False:
+        rx = rando.randint(0, width - 2)
+        ry = rando.randint(0, height - 1)
+        if grid[ry][rx] & 2:
+            grid[ry][rx] -= 2
+            grid[ry][rx + 1] -= 8
+    return grid
